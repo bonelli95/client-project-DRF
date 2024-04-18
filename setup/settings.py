ffc4699fe3f510de0c4ff66455a8cf372e5cd79d
Rodrigo Bonelli <159248066+bonelli95@users.noreply.github.com>
2024-04-18 08:49:21 +0100

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -24,8 +24,6 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-USE_X_FORWARDED_HOST = True
-
 ALLOWED_HOSTS = ['*']
 
 
